c76d033d55306cce6c614defd61013ae3b58071a
RogerJhw <roger@mapventures.org>
2025-07-29 09:58:47 -0400

    Update evans_app.py

--- evans_app.py
+++ evans_app.py
@@ -196,12 +196,12 @@ with col1:
 
         output = generate_and_display_clip(user_url)
         my_ipython_html_object = output
-    if my_ipython_html_object and hasattr(my_ipython_html_object, '_repr_html_'):
-        raw_html_content = my_ipython_html_object._repr_html_()
-        st.session_state.arts.append(raw_html_content)
-    else:
-        st.error("Unable to generate HTML preview for the given article.")
-    # Display your articles here
+        if my_ipython_html_object and hasattr(my_ipython_html_object, '_repr_html_'):
+            raw_html_content = my_ipython_html_object._repr_html_()
+            st.session_state.arts.append(raw_html_content)
+        else:
+            st.error("Unable to generate HTML preview for the given article.")
+        # Display your articles here
     for i, art in enumerate(arts, 1):
         st.write(f'**Article {i}**')
         components.html(art, height=300, scrolling=True)
